45fe4c6c7bb4b5e8a2806c54b6fc9bebfa25916d
somyaasthana27 <56486403+somyaasthana27@users.noreply.github.com>
2019-10-12 23:36:56 +0530

    Add files via upload

---
+++ add two number.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,sum;
+printf("enter the values for a Aand B \n");
+scanf("%d \n",&a);
+scanf("%d",&b);
+sum=a+b;
+printf("the sum of two number= %d",sum);
+}
